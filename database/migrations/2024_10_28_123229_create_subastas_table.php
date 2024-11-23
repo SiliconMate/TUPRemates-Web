3aa7a1109b0fb7ea1e21365d57be935f0a80c99d
FranM4rt <mjuanfrancisco45@gmail.com>
2024-11-23 14:51:31 -0300

    agregar soporte para eliminacion logica en la tabla de subastas

--- database/migrations/2024_10_28_123229_create_subastas_table.php
+++ database/migrations/2024_10_28_123229_create_subastas_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
                 ->onUpdate('cascade')
                 ->onDelete('no action');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
